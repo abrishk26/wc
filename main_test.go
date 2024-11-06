34cb65b2b0a796227e543d3114f54a7c595d350f
Abreham Kassa <abrehamkassa19@gmail.com>
2024-11-06 15:50:06 +0300

    Add a test function to test the lines counting.

--- main_test.go
+++ main_test.go
@@ -9,7 +9,19 @@ func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 
 	exp := 4
-	res := count(b)
+	res := count(b, false)
+
+	if res != exp {
+		t.Errorf("expected %d, got %d instead.", exp, res)
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1")
+
+	exp := 3
+
+	res := count(b, true)
 
 	if res != exp {
 		t.Errorf("expected %d, got %d instead.", exp, res)

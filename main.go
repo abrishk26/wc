46d2fe97f5099b265654626f54506ae1856af2b5
Abreham Kassa <abrehamkassa19@gmail.com>
2024-11-06 15:45:55 +0300

    Modified the count function to be able to count lines in addition to words.

--- main.go
+++ main.go
@@ -5,20 +5,29 @@ import (
 	"io"
 	"fmt"
 	"os"
+	"flag"
 )
 
 func main() {
+
+	lines := flag.Bool("l", false, "Count line")
+
+	// parsing flags provided by the user
+	flag.Parse()
+
 	// Calling the count function to count the number of words
 	// received from the Standard Input and printing it out.
-	fmt.Println(count(os.Stdin))
+	fmt.Println(count(os.Stdin, *lines))
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
 	// A scanner is used to read a text from a Reader (such as file).
 	scanner := bufio.NewScanner(r)
 
-	// define the scanner split type to words. (default is split by lines)
-	scanner.Split(bufio.ScanWords)
+	if !countLines {
+		// define the scanner split type to words. (default is split by lines)
+		scanner.Split(bufio.ScanWords)
+	}
 
 	// Defining word counter
 	wc := 0
